098704bd7a859698602999da7e7b96f0ca0a945b
Stephen Dawes <sdawes@outlook.com>
2016-05-25 20:32:10 +0100

    Ch15_Single Responsibility Principle

--- feature_test.rb
+++ feature_test.rb
@@ -1,14 +1,6 @@
 require '~/documents/projects/boris-bikes/lib/docking_station.rb'  # => true
 require '~/documents/projects/boris-bikes/lib/bike.rb'             # => false
 
-docking_station = DockingStation.new  # => #<DockingStation:0x007f9d93936e98 @bikes=[]>
-20.times do                           # => 20
-  docking_station.dock(Bike.new)      # => [#<Bike:0x007f9d93936ab0>], [#<Bike:0x007f9d93936ab0>, #<Bike:0x007f9d939368a8>], [#<Bike:0x007f9d93936ab0>, #<Bike:0x007f9d939368a8>, #<Bike:0x007f9d93936678>], [#<Bike:0x007f9d93936ab0>, #<Bike:0x007f9d939368a8>, #<Bike:0x007f9d93936678>, #<Bike:0x007f9d939363f8>], [#<Bike:0x007f9d93936ab0>, #<Bike:0x007f9d939368a8>, #<Bike:0x007f9d93936678>, #<Bike:0x007f9d939363f8>, #<Bike:0x007f9d93936128>], [#<Bike:0x007f9d93936ab0>, #<Bike:0x007f9d939368a8>, #<Bike:0x007f9d93936678>, #<Bike:0x007f9d939363f8>, #<Bike:0x007f9d93936128>, #<Bike:0x007f9d93935e08>], [#<Bike:0x007f9d93936ab0>, #<Bike:0x007f9d939368a8>, #<Bike:0x007f9d93936678>, #<Bike:0x007f9d939363f8>, #<Bike:0x007f9d93936128>, #<Bike:0x007f9d93935e08>, #<Bike:0x007f9d93935a98>], [#<Bike:0x007f9d93936ab0>, #<Bike:0x007f9d939368a8>, #<Bike:0x007f9d93936678>, #<Bike:0x007f9d939363f8>, #<Bike:0x007f9d93936128>, #<Bike:0x007f9d93935e08>, #<Bike:0x007f9d93935a98>, #<Bike:0x007f9d939356d8>], [#...
-  end                                   # => 20
-docking_station.dock(Bike.new)        # ~> RuntimeError: Docking station full
-
-# ~> RuntimeError
-# ~> Docking station full
-# ~>
-# ~> /Users/sdawes/documents/projects/boris-bikes/lib/docking_station.rb:14:in `dock'
-# ~> /Users/sdawes/Documents/projects/boris-bikes/feature_test.rb:8:in `<main>'
+docking_station = DockingStation.new         # => #<DockingStation:0x007f83bba66b70 @bikes=[]>
+18.times { docking_station.dock(Bike.new) }  # => 18
+docking_station.dock(Bike.new)               # => [#<Bike:0x007f83bba66918>, #<Bike:0x007f83bba668f0>, #<Bike:0x007f83bba668c8>, #<Bike:0x007f83bba668a0>, #<Bike:0x007f83bba66878>, #<Bike:0x007f83bba66850>, #<Bike:0x007f83bba66828>, #<Bike:0x007f83bba66800>, #<Bike:0x007f83bba667d8>, #<Bike:0x007f83bba667b0>, #<Bike:0x007f83bba66788>, #<Bike:0x007f83bba66760>, #<Bike:0x007f83bba66738>, #<Bike:0x007f83bba66710>, #<Bike:0x007f83bba666e8>, #<Bike:0x007f83bba666c0>, #<Bike:0x007f83bba66698>, #<Bike:0x007f83bba66670>, #<Bike:0x007f83bba664b8>]
